b40c0ea1fa0eb4aabb92b4a388dfc39d3fc45446
Tomás Senart <tomas@sourcegraph.com>
2019-05-06 14:13:48 +0200

    repo-updater: Implement Exclude for Gitolite

--- cmd/repo-updater/repos/types.go
+++ cmd/repo-updater/repos/types.go
@@ -101,6 +101,8 @@ func (e *ExternalService) Exclude(rs ...*Repo) error {
 		return e.excludeGitLabRepos(rs...)
 	case "bitbucketserver":
 		return e.excludeBitbucketServerRepos(rs...)
+	case "gitolite":
+		return e.excludeGitoliteRepos(rs...)
 	case "other":
 		return e.updateOtherRepos(false, rs...)
 	default:
@@ -284,6 +286,40 @@ func (e *ExternalService) excludeBitbucketServerRepos(rs ...*Repo) error {
 					set[name] = true
 				}
 			}
+		}
+
+		return "exclude", c.Exclude, nil
+	})
+}
+
+// excludeGitoliteRepos changes the configuration of a Gitolite external service to exclude the
+// given repos from being synced.
+func (e *ExternalService) excludeGitoliteRepos(rs ...*Repo) error {
+	if len(rs) == 0 {
+		return nil
+	}
+
+	return e.config("gitolite", func(v interface{}) (string, interface{}, error) {
+		c := v.(*schema.GitoliteConnection)
+		set := make(map[string]bool, len(c.Exclude))
+		for _, ex := range c.Exclude {
+			if ex.Name != "" {
+				set[ex.Name] = true
+			}
+		}
+
+		for _, r := range rs {
+			if strings.ToLower(r.ExternalRepo.ServiceType) != "gitolite" {
+				continue
+			}
+
+			name := strings.TrimPrefix(r.Name, c.Prefix)
+			if name == "" || set[name] {
+				continue
+			}
+
+			c.Exclude = append(c.Exclude, &schema.ExcludedGitoliteRepo{Name: name})
+			set[name] = true
 		}
 
 		return "exclude", c.Exclude, nil
